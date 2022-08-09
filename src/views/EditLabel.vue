041c78324c2cbe51ba83b17428eb3efb516200e3
bachelor98 <2596445436@qq.com>
2022-08-09 11:03:35 +0800

    修复了标签无法存进localstorage的bug

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,13 +1,15 @@
 <template>
     <Layout>
-        
+
     </Layout>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 export default class EditLabel extends Vue{
-    
+    create(){
+        console.log(this.$route.params)
+    }
 }
 </script>
 
