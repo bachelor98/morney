7192b28a187029eedd616c1553727c4642c648a8
bachelor98 <2596445436@qq.com>
2022-08-10 10:18:24 +0800

    使用全局tagList

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -5,3 +5,20 @@ type RecordItem={
     amount:number,
     createAt?:Date  //Date是类，或者说是构造函数。?:是指还可能是undefined类型
 }
+
+type Tag = {
+    id:string;
+    name:string;
+}
+type TagListModel = {
+    data:Tag[]
+    fetch:()=>Tag[]
+    create:(name:string)=>'success' | 'duplicated' | 'null'   //联合类型，success表示成功，duplicated表示内容重复
+    update:(id:string,name:string)=>'success' | 'not found' | 'duplicated'
+    remove:(id:string)=>boolean
+    save:()=>void
+}
+
+interface Window{
+    tagList:Tag[]
+}
--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,15 @@ import store from './store'
 import Nav from '@/components/Nav.vue'
 import Layout from '@/components/Layout.vue'
 import Icon from '@/components/Icon.vue'
+import tagListModel from './models/tagListModel'
 
 Vue.config.productionTip = false
 
 Vue.component('Nav',Nav) //全局组件
 Vue.component('Layout',Layout)
 Vue.component('Icon',Icon)
+
+window.tagList = tagListModel.fetch() //全局变量
 
 new Vue({
   router,
